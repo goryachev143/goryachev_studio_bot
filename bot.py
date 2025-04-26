2d469dfb4d6a5557db3ef3eb47b12083ab6f8602
goryachev143 <nvision74@mail.ru>
2025-04-26 12:48:43 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,13 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
 import os
+from aiogram import Bot, Dispatcher, executor, types
 
-API_TOKEN = os.getenv("BOT_TOKEN")
+API_TOKEN = os.getenv("API_TOKEN")
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=["start"])
-async def welcome(message: types.Message):
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
     await message.reply("Привет! Я бот Goryachev Studio!")
 
-if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+if __name__ == '__main__':
